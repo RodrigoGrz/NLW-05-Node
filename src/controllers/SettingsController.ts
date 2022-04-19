c4df4baf47cf883a68c4ed59307c84d982b38453
RodrigoRiv182 <rodrigo.gregorio1@hotmail.com>
2022-04-19 14:51:19 -0300

    Settings Service

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
     async create(request: Request, response: Response) {
         const { chat, username } = request.body;
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const setting = settingsRepository.create({
-            chat,
-            username
-        });
-    
-        await settingsRepository.save(setting);
-    
-        return response.json(setting);
+        const settingsService = new SettingsService();
+
+        try {
+            const setting = await settingsService.create({ chat, username });
+
+            return response.json(setting);
+        } catch (err) {
+            return response.status(400).json({
+                message: err.message
+            })
+        }
     }
 }
 
